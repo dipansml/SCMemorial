501f7299a3ef407e8a2b248352405557c2d7ef81
dipan <dipan.das@beas.co.in>
2026-04-15 13:03:57 +0530

    test

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -58,6 +58,8 @@ const Login = ({ navigation }: LoginProps) =>  {
   console.log("Forgot pass click")
 };
 
+//Test
+
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.content}>

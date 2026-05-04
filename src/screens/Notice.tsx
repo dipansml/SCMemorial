71a32432b87f4a3a3e1b27e22d2e074fa7f01aab
Beas <dipan.das@beas.co.in>
2026-05-04 16:17:01 +0530

    Notification commit

--- src/screens/Notice.tsx
+++ src/screens/Notice.tsx
@@ -3,8 +3,43 @@ import { View, StyleSheet } from 'react-native';
 import AppHeader from '../component/AppHeader';
 import { openParentDrawer } from '../navigation/navigationRef';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import NoticeList from '../component/NoticeComponent';
+import { NoticeItem } from '../component/NoticeCard';
+
+interface State {
+  notices: NoticeItem[];
+}
+
 
 const Notice = () => {
+  const [state] = React.useState<State>({
+    notices: [
+      {
+        id: '1',
+        important: true,
+        title: 'Emergency Campus Maintenance',
+        description:
+          'The East Wing of the campus will be closed for emergency maintenance from Friday evening Sunday.',
+        date: 'Oct 24, 2023',
+      },
+      {
+        id: '2',
+        important: true,
+        title: 'Final Semester Lab Schedule',
+        description:
+          'The final semester laboratory examinations for Computer Science and Engineering students have...',
+        date: 'Oct 22, 2023',
+      },
+      {
+        id: '3',
+        important: false,
+        title: 'Winter Break Announcement',
+        description:
+          'Official dates for the upcoming winter vacation.',
+        date: 'Oct 24, 2023',
+      },
+    ],
+  });
   return (
     <SafeAreaView style={styles.container}>
       
@@ -16,7 +51,7 @@ const Notice = () => {
       />
 
       <View style={styles.content}>
-        {/* Your screen content */}
+        <NoticeList data={state.notices} />
       </View>
 
     </SafeAreaView>
